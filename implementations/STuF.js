d907671548b9ce5e091de7e5a0b1e68fa720bb41
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-07-16 17:29:37 -0400

    move to class

--- implementations/STuF.js
+++ implementations/STuF.js
@@ -1,91 +1,102 @@
-// This project uses STuF v1.0
-// to encode and decode links.
-// 
-// Info and licenses:
-// github.com/stuffyerface/STuF
+// This Library uses STuF
+// https://github.com/stuffyerface/STuF
 
-function stufDecode(encoded) {
-  if (!encoded.startsWith('l$')) {
-    throw new Error('Invalid encoded string');
-  }
-  let prefix = encoded[2];
-  let suffix = encoded[3];
-  let dotIndices = encoded.slice(4, encoded.indexOf('|')).split('').map(Number);
-  let urlBody = encoded.slice(encoded.indexOf('|') + 1);
 
-  let first9 = urlBody.slice(0, 9 - dotIndices.length);
-  let then = urlBody.slice(9 - dotIndices.length).replace(/\^/g, '.');
-
-  let url = first9 + then;
-  url = charInc(url,-1)
-
-  dotIndices.forEach((index) => {
-    url = url.slice(0, index) + '.' + url.slice(index);
-  });
-
-  if (prefix === 'h') {
-    url = 'http://' + url;
-  } else if (prefix === 'H') {
-    url = 'https://' + url;
-  }
-
-  if (suffix === '1') {
-    url += '.png';
-  } else if (suffix === '2') {
-    url += '.jpg';
-  } else if (suffix === '3') {
-    url += '.jpeg';
-  } else if (suffix === '4') {
-    url += '.gif';
-  }
-
-  return url;
-}
-
-function stufEncode(url) {
-  let encoded = "l$"
-  if (url.startsWith('http://')) {
-    encoded += 'h';
-    url = url.slice(7);
-  } else if (url.startsWith('https://')) {
-    encoded += 'H';
-    url = url.slice(8);
-  }
-
-  if (url.endsWith('.png')) {
-    encoded += '1';
-    url = url.slice(0, -4);
-  } else if (url.endsWith('.jpg')) {
-    encoded += '2';
-    url = url.slice(0, -4);
-  } else if (url.endsWith('.jpeg')) {
-    encoded += '3';
-    url = url.slice(0, -5);
-  } else if (url.endsWith('.gif')) {
-    encoded += '4';
-    url = url.slice(0, -4);
-  } else {
-    encoded += '0';
-  }
-
-  let dotIndices = [];
-  for (let i = 0; (i < url.length) && (i <= 9); i++) {
-    if (url[i] === '.') {
-      dotIndices.push(i);
-      if (dotIndices.length === 9) break;
+export default class STuFLib {
+  /**
+   * Decodes a string in Standardized Truncated url Format.
+   * @param {String} string - The String to Decode.
+   */
+  static decode = (string) => {
+    if (!string.startsWith('l$')) {
+      throw new Error('String does not appear to be in STuF');
     }
+    let prefix = string[2];
+    let suffix = string[3];
+    let dotIndices = string.slice(4, string.indexOf('|')).split('').map(Number);
+    let urlBody = string.slice(string.indexOf('|') + 1);
+
+    let first9 = urlBody.slice(0, 9 - dotIndices.length);
+    let then = urlBody.slice(9 - dotIndices.length).replace(/\^/g, '.');
+
+    let url = first9 + then;
+    url = charInc(url, -1)
+
+    // Restore the dots in the first part of the URL
+    dotIndices.forEach((index) => {
+      url = url.slice(0, index) + '.' + url.slice(index);
+    });
+
+    // Add the prefix back
+    if (prefix === 'h') {
+      url = 'http://' + url;
+    } else if (prefix === 'H') {
+      url = 'https://' + url;
+    }
+
+    // Add the suffix back
+    if (suffix === '1') {
+      url += '.png';
+    } else if (suffix === '2') {
+      url += '.jpg';
+    } else if (suffix === '3') {
+      url += '.jpeg';
+    } else if (suffix === '4') {
+      url += '.gif';
+    }
+
+    return url;
+  };
+
+  /**
+   * Encodes a string in Standardized Truncated url Format.
+   * @param {String} url - The URL to Encode.
+   */
+  static encode = (url) => {
+    let encoded = "l$"
+    if (url.startsWith('http://')) {
+      encoded += 'h';
+      url = url.slice(7); // Remove the 'http://' part
+    } else if (url.startsWith('https://')) {
+      encoded += 'H';
+      url = url.slice(8); // Remove the 'https://' part
+    }
+
+    if (url.endsWith('.png')) {
+      encoded += '1';
+      url = url.slice(0, -4); // Remove the '.png' part
+    } else if (url.endsWith('.jpg')) {
+      encoded += '2';
+      url = url.slice(0, -4); // Remove the '.jpg' part
+    } else if (url.endsWith('.jpeg')) {
+      encoded += '3';
+      url = url.slice(0, -5); // Remove the '.jpeg' part
+    } else if (url.endsWith('.gif')) {
+      encoded += '4';
+      url = url.slice(0, -4); // Remove the '.gif' part
+    } else {
+      encoded += '0';
+    }
+
+    let dotIndices = [];
+    for (let i = 0; (i < url.length) && (i <= 9); i++) {
+      if (url[i] === '.') {
+        dotIndices.push(i);
+        if (dotIndices.length === 9) break; // Stop after 9 dots
+      }
+    }
+
+    let first9 = url.substring(0, 9)
+    let then = url.substring(9).replace(/\./g, '^');
+    first9 = first9.replace(/\./g, '');
+    let shifted = charInc(first9 + then, 1)
+
+    encoded += dotIndices.map(index => index.toString()).join('') + '|';
+    encoded += shifted
+
+
+    return encoded;
   }
-
-  let first9 = url.substring(0, 9)
-  let then = url.substring(9).replace(/\./g, '^');
-  first9 = first9.replace(/\./g, '');
-  let shifted = charInc(first9+then,1)
-
-  encoded += dotIndices.map(index => index.toString()).join('') + '|';
-  encoded += shifted
-
-
-  return encoded;
 }
 
 function charInc(str, int) {
@@ -96,9 +107,11 @@ function charInc(str, int) {
     let index = charSet.indexOf(char);
 
     if (index !== -1) {
+      // Get the next character in the set, wrap around using modulo operator
       let nextChar = charSet[(index + int) % charSet.length];
       incrementedStr += nextChar;
     } else {
+      // If the character is not in the set, keep it unchanged
       incrementedStr += char;
     }
   }
